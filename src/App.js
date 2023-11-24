d10bbf53c40f2f208a52b37c3a2044d1c520a3b8
Felipe Domiciano <120732088+Domfelipe96@users.noreply.github.com>
2023-11-23 22:42:32 -0300

    add buttons e style para algumas funcionalidades do painel

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleBotao1 = () => {
+    alert('1º Botão clicado!');
+    // Adicione sua lógica personalizada para o 1º Botão aqui
+  };
+
+  const handleBotao2 = () => {
+    alert('2º Botão clicado!');
+    // Adicione sua lógica personalizada para o 2º Botão aqui
+  };
+
+  const handleBotao3 = () => {
+    alert('3º Botão clicado!');
+    // Adicione sua lógica personalizada para o 3º Botão aqui
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div style={{ background: 'black', width: '50%', height: 100, justifyContent: 'end', alignContent: 'end', flexDirection: 'row'}}>
-          <p> O felipe eh programador</p>
-          <p> O felipe eh programador</p>
-        </div>
-        <p>
-          Hello <code>World</code> and Felipe
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Menubar'>
+        <img className='Imagem' src="logo.png" alt="Logo"></img>
+        <button onClick={handleBotao1}>1º Botão</button>
+        <button onClick={handleBotao2}>2º Botão</button>
+        <button onClick={handleBotao3}>3º Botão</button>
+      </div>
+      <div className='essential'>
+        <p>ESPAÇO RESERVADO PARA COLOCAR O VÍDEO DE APRESENTAÇÃO E OS DADOS PRINCIPAIS</p>
+      </div>
+      <div className='additional'>
+        <p>ESPAÇO RESERVADO PARA COLOCAR ALGUNS DADOS ADICIONAIS E AVALIAÇÕES DE USUÁRIOS</p>
+      </div>
+      <div className='explanation'>
+        <p>ESPAÇO RESERVADO PARA COLOCAR UM TUTORIAL E BREVE EXPLICAÇÃO SOBRE COMO FUNCIONA O APP</p>
+      </div>
+      <div className='director'>
+        <p>ESPAÇO RESERVADO PARA COLOCAR OS LINKS DE DIRECIONAMENTO DAS REDES SOCIAIS</p>
+      </div>
     </div>
   );
 }
+
 
 export default App;
