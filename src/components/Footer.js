79317a636fe1f71c1c1d14516aeafa40f296e5aa
Felipe Domiciano <120732088+Domfelipe96@users.noreply.github.com>
2024-02-02 11:26:44 -0300

    atualizando versão para postagem

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,90 @@
 // Footer.js
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div>
-                <h4>Siga-nos nas redes sociais:</h4>
-                <ul>
-                    <li>
-                        <a href="https://www.facebook.com/xpuntoapp" target="_blank" rel="noopener noreferrer">
-                            Facebook
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-                            Twitter
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/xpuntoapp/" target="_blank" rel="noopener noreferrer">
-                            Instagram
-                        </a>
-                    </li>
-                    {/* Adicione outros links de redes sociais conforme necessário */}
-                </ul>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div>
+        <h4>Informações importantes:</h4>
+        <ul>
+          <li>
+            <a
+              href="https://sergiodellerba.github.io/xpunto-txt-sobre/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Sobre
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://sergiodellerba.github.io/xpunto-txt-faq/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              FAQ
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://sergiodellerba.github.io/xpunto-dicas-seguranca/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Segurança
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://sergiodellerba.github.io/xpunto-txt-termos-uso/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Termos de uso
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://sergiodellerba.github.io/xpunto-txt-politica-privacidade/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LGPD
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/xpuntoapp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/xpuntoapp/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          {/* Adicione outros links de redes sociais conforme necessário */}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
